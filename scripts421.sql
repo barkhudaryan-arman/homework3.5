2781968b720b2e06aae0947d6d39274fe626386d
barkhudaryan-arman <armanik147@gmail.com>
2024-11-06 22:38:17 +0300

    homework 4.2 №2 исправил

--- scripts421.sql
+++ scripts421.sql
@@ -3,8 +3,7 @@ ADD CONSTRAINT chk_student_age CHECK (age >= 16);
 
 ALTER TABLE Student
 ADD CONSTRAINT uq_student_name UNIQUE (name),
-ADD CONSTRAINT uq_student_name_age UNIQUE (name, age);
-ADD CONSTRAINT nn_student_name UNIQUE (IS NOT NULL);
+ALTER COLUMN name SET NOT NULL;
 
 ALTER TABLE Faculty
 ADD CONSTRAINT uq_faculty_name_color UNIQUE (name, color);
